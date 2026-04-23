9a068f62579c0efd7b0670729a2f0bba86c4d294
Arnaud Jopart <arnaudjopart@users.noreply.github.com>
2026-04-23 16:59:49 +0200

    end of day

--- Assets/_Project/Scripts/ImageDetector.cs
+++ Assets/_Project/Scripts/ImageDetector.cs
@@ -1,23 +1,78 @@
+using System;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
 public class ImageDetector : MonoBehaviour
 {
     private ARTrackedImageManager _trackedImageManager;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         _trackedImageManager = GetComponent<ARTrackedImageManager>();
     }
 
+    private void OnEnable()
+    {
+       _trackedImageManager.trackablesChanged.AddListener(OnChanged);
+    }
+
+    private void OnDisable()
+    {
+        _trackedImageManager.trackablesChanged.RemoveListener(OnChanged);
+    }
+
+    private void OnChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
+    {
+        foreach (ARTrackedImage image in eventArgs.added)
+        {
+            string imageName = image.referenceImage.name;
+            Vector3 spawnPosition = image.transform.position;
+            Debug.Log("Image Added: "+imageName);
+            switch (imageName)
+            {
+                case "one":
+                    GameObject cube =  GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.SetParent(image.transform, false);
+                    cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    
+                    break;
+                case "Rafflesia":
+                    GameObject sphere =  GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    sphere.transform.SetParent(image.transform,false);
+                    sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    
+                    break;
+                default:
+                    Debug.Log("Prefab Not Found");
+                    break;
+                
+            }
+            
+            //continue
+            
+            
+        }
+
+        foreach (ARTrackedImage image in eventArgs.updated)
+        {
+            
+        }
+
+        foreach (var image in eventArgs.removed)
+        {
+            Debug.Log("Image Removed");
+        }
+
+
+    }
+
+
     // Update is called once per frame
     void Update()
     {
         
     }
+    
 
-    public void ReactToTrackableChanges(ARTrackedImagesChangedEventArgs eventArgs)
-    {
-        Debug.Log("ARTrackedImagesChanged");
-    }
 }
